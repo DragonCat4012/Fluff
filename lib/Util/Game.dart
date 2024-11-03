bb5bcca59756c405044e8dd711828bc3c6b4dabb
Kiara <frida.shivaa@gmail.com>
2024-11-03 16:00:10 +0100

    add game ids

--- lib/Util/Game.dart
+++ lib/Util/Game.dart
@@ -1,12 +1,18 @@
-
+import 'package:uuid/uuid.dart';
 import 'package:fluff/Util/LogEntry.dart';
 
 class Game {
    List<LogEntry> log = List.empty();
+   String game_uuid = "";
 
    Game(){
      log = List.empty();
+     const uuid = Uuid();
+     game_uuid = uuid.v1().toString();
    }
+
+
+   Game.fromParam(this.log, this.game_uuid);
 
   void addEntry(LogEntry entry) {
      log = [entry, ...log];
@@ -16,7 +22,15 @@ class Game {
    String toJson() {
      List<String> entries = List.empty();
      log.forEach((element) {entries.add(element.toJson());});
+     return '{uuid: $game_uuid, log: [${entries.join(", ")}]}';
+   }
 
-     return '{uid: "xxx", log: [${entries.join(", ")}]}';
+   static Game fromJson(model) {
+     return Game.fromParam(model["game_uuid"], model["log"]);
+   }
+
+   @override
+   String toString() {
+     return "($game_uuid: ${log.length})";
    }
 }
